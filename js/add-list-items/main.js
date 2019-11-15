3d0795630159a5cb9964f94b3c60fe9776ddd7d4
Kristiyan Krumov <kristiyan.boyanov@gmail.com>
2019-11-15 08:16:15 +0200

    Add JS files

---
+++ js/add-list-items/main.js
@@ -0,0 +1,24 @@
+let textInputElement = document.querySelector("#text-input")
+let buttonElement = document.querySelector("#add-button")
+let listElement = document.querySelector("#list")
+
+// Добавете eventListener към buttonElement, който при кликване да изпълнява 
+// функцията addListItem
+
+
+
+// Довършете фукнцията, така че изпълнението й да добавя нов li елемент към listElement
+// текстът на listElement да е стойността, въведена в textInputElement
+// новият li елемент трябва да има CSS клас "styled"
+function addListItem(event){
+
+}
+
+// Добавете eventListener към textInputElement, който при натискане на копче от клавиатурата да изпълнява 
+// функцията onKeyPress
+
+
+// addListItemOnEnter създава нов li елемент, добавя го към listElement само ако потребителят е натиснал Enter
+function addListItemOnEnter(event){
+
+}
